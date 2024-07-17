03ade69e949e58115ed4acb198dcd7f2db45da08
gmumdal <shinhs6384@naver.com>
2024-07-17 16:33:09 +0900

    fix: client method edit

--- Client.cpp
+++ Client.cpp
@@ -1,8 +1,7 @@
 
 #include "Client.hpp"
 
-Client::Client(int fd) : _fd(fd)
-{
+Client::Client(int fd) : _fd(fd) {
 	struct sockaddr_in client_addr;
 	socklen_t addr_len = sizeof(client_addr);
 
@@ -13,38 +12,31 @@ Client::Client(int fd) : _fd(fd)
 Client::~Client() {}
 
 
-void Client::setNickname(std::string const &nickname)
-{
+void Client::setNickname(std::string const &nickname) {
 	_nickname = nickname;
 }
 
-void Client::setUsername(std::string const &username)
-{
+void Client::setUsername(std::string const &username) {
 	_username = username;
 }
 
-void Client::setRealname(std::string const &realname)
-{
+void Client::setRealname(std::string const &realname) {
 	_realname = realname;
 }
 
-void Client::setRecvBuf(std::string message)
-{
+void Client::setRecvBuf(std::string message) {
 	_recv_buffer.append(message);
 }
 
-void Client::setSendBuf(std::string message)
-{
+void Client::setSendBuf(std::string message) {
 	_send_buffer.append(message);
 }
 
-std::string Client::getNickname() const
-{
+std::string Client::getNickname() const {
 	return _nickname;
 }
 
-std::string Client::getUsername() const
-{
+std::string Client::getUsername() const {
 	return _username;
 }
 
@@ -56,48 +48,39 @@ std::string Client::getRealname() const {
 	return _realname;
 }
 
-int Client::getFd() const
-{
+int Client::getFd() const {
 	return _fd;
 }
 
-std::string Client::getRecvBuf() const
-{
-	return _recv_buffer;
+std::string Client::getRecvBuf() const {
+	return _recv_buffer.substr(0, _recv_buffer.find("\r\n") + 2);
 }
 
-std::string Client::getSendBuf() const
-{
+std::string Client::getSendBuf() const {
 	return _send_buffer;
 }
 
-void Client::clearRecvBuf()
-{
-	_recv_buffer = _recv_buffer.substr(_recv_buffer.find("\n") + 1, _recv_buffer.size());
+void Client::clearRecvBuf() {
+	_recv_buffer = _recv_buffer.substr(_recv_buffer.find("\r\n") + 2, _recv_buffer.size());
 }
 
-void Client::clearSendBuf()
-{
-	_send_buffer = _send_buffer.substr(_send_buffer.find("\n") + 1, _send_buffer.size());
+void Client::clearSendBuf() {
+	_send_buffer = _send_buffer.substr(_send_buffer.find("\r\n") + 2, _send_buffer.size());
 }
 
-int Client::recvSocket()
-{
+int Client::recvSocket() {
 	char buf[BUFFER_SIZE] = {0};
-	if (recv(_fd, buf, sizeof(buf), 0) <= 0)
-	{
+	if (recv(_fd, buf, sizeof(buf), 0) <= 0) {
 		return EOF;
 	}
 	setRecvBuf(buf);
-	if (_recv_buffer.find("\n") != std::string::npos)
-	{
+	if (_recv_buffer.find("\r\n") != std::string::npos) {
 		return END;
 	}
 	return CONTINUE;
 }
 
-int Client::sendSocket()
-{
+int Client::sendSocket() {
 	int size = send(_fd, _send_buffer.c_str(), _send_buffer.size(), 0);
 	_send_buffer.erase(0, size);
 	if (_send_buffer.size() == 0)
