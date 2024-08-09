a346411d5af8c485150ad4298c5c01f63933f980
gmumdal <shinhs6384@naver.com>
2024-08-09 18:05:56 +0900

    feat: add noncommand

--- Server.cpp
+++ Server.cpp
@@ -128,6 +128,8 @@ void Server::parsing(Client *clnt) {
 			executor.modeCommand();
 		} else if (command == "PRIVMSG") {
 			executor.privmsgCommand();
+		} else {
+			executor.nonCommand();
 		}
 		std::cout << clnt->getSendBuf();
 	}
